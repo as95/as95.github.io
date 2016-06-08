72e0bdfb2da22729fe693158d65267ba152b48a5
Amit Sen <asen@email.arizona.edu>
2016-06-07 23:41:18 -0700

    Add new tweaks

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
 
-  
+
+$(".toggle_container").hide(); 
+$("button.reveal").click(function(){
+    $(this).toggleClass("active").next().slideToggle("fast");
+    
+    if ($.trim($(this).text()) === 'About') {
+        $(this).text('Less');
+    } else {
+        $(this).text('About');        
+    }
+    
+    return false; 
+});
+ $("a[href='" + window.location.hash + "']").parent(".reveal").click();
     $('.smoothScroll').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
