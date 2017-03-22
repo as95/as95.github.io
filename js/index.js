29e0f3a74e6bb2206f446fd2a8689b9c99a93506
Amit Sen <asen@email.arizona.edu>
2017-03-22 05:03:01 -0700

    Add functionality to portfolio item divs

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
 $(document).ready(function(){
- //$('.navigation').hide();
+ ///$('.navigation').hide();
  //$('.frontpage').hide();
  //$('.work').hide();
- ///$('.about').hide();
+ //$('.about').hide();
  //$('.contact').hide();
+  
+  
+  function getCurrClass (portItem) {
+    if(portItem.hasClass('planets')){
+      return '.planets';
+    }else if(portItem.hasClass('arizona-sase')){
+      return '.arizona-sase';
+    }else if(portItem.hasClass('loading-deck')){
+      return '.loading-deck';
+    }else if(portItem.hasClass('wiki-viewer')){
+      return '.wiki-viewer';
+    }else{
+      return '.vpl';
+    }
+   
+  };
+  
+  $('.portfolio-content').hide();
+  
+  $('.portfolio-item').click(function(){
+    var currClass = getCurrClass($(this));
+    var currContent = currClass + '-content';
+     
+    $(currClass).toggleClass('open');
+    $(currClass).toggleClass('closed');
+    setTimeout(function(){
+      $(currContent).slideToggle();
+    },500);
+    
+  });
+  
+  
+  
   
   
   $(window).scroll(function(){
@@ -14,13 +47,11 @@ $(document).ready(function(){
       $('.navigation').css('background-color','transparent');
       $('.nav-link').css('color','rgba(0, 0, 0, 1)');
       $('.siteTitle').css('color', 'black');
-      $('.siteTitle').css('font-weight', '600');
       $('.fa-bars').css('color', 'black');
     } else if (scrollLocation > 30) {
       $('.navigation').css('background-color','rgba(0, 0, 0, .9)');
       $('.nav-link').css('color','rgba(255, 255, 255, 1)');
       $('.siteTitle').css('color', 'white');
-      $('.siteTitle').css('font-weight', '400');
       $('.fa-bars').css('color', 'white');
     }
   });
